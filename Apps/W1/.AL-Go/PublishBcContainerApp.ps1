30613ddcdf6bf4643f5733ca556f9be2f83eab5b
freddydk <freddyk@microsoft.com>
2022-05-31 09:37:09 +0200

    publish

--- Apps/W1/.AL-Go/PublishBcContainerApp.ps1
+++ Apps/W1/.AL-Go/PublishBcContainerApp.ps1
@@ -7,15 +7,15 @@ Publish-BcContainerApp @parameters
 $filename = [System.IO.Path]::GetFileName($parameters.appFile)
 if ($filename -like "Microsoft_System Application_*.*.*.*.app") {
     Write-Host "Publishing Base Application"
-    $parameters.appFile = ":C:\Run\my\Microsoft_Base Application.app"
+    $parameters.appFile = Join-Path $bcContainerHelperConfig.hostHelperFolder "Extensions\$($parameters.ContainerName)\my\Microsoft_Base Application.app"
     Publish-BcContainerApp @parameters
 
     Write-Host "Publishing Application"
-    $parameters.appFile = ":C:\Run\my\Microsoft_Application.app"
+    $parameters.appFile = Join-Path $bcContainerHelperConfig.hostHelperFolder "Extensions\$($parameters.ContainerName)\my\Microsoft_Application.app"
     Publish-BcContainerApp @parameters
 }
 elseif ($filename -like "Microsoft_System Application Test_*.*.*.*.app") {
     Write-Host "Publishing Tests-TestLibraries"
-    $parameters.appFile = ":C:\Run\my\Microsoft_Tests-TestLibraries.app"
+    $parameters.appFile = Join-Path $bcContainerHelperConfig.hostHelperFolder "Extensions\$($parameters.ContainerName)\my\Microsoft_Tests-TestLibraries.app"
     Publish-BcContainerApp @parameters
 }
